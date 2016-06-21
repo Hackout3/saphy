d613402df82392e8b1d07f5972b7fa921f8bca63
Simon Frost <sdwfrost@gmail.com>
2016-06-21 23:37:18 +0100

    Fixed exports

--- R/timeprune.R
+++ R/timeprune.R
@@ -1,14 +1,19 @@
 #' Prune tree in time order
-#' 
+#'
 #' \code{timeprune} prunes a tree by removing tips one at a time, in reverse time order.
 #' If the time is omitted and the tree is rooted, then the root-to-tip distance is used as
 #' a proxy of sampling time.
-#' 
+#'
 #' @param tr a tree, as a \code{phylo} object
 #' @param tipdates a vector of sampling times or dates
 #' @param reverse a boolean (default: \code{FALSE}) determining whether the list of trees should be returned
 #' in reverse time
-#' 
+#'
+#' @return A list with tipdates and trees.
+#'
+#' @author Simon Frost (\email{sdwfrost@@gmail.com})
+#'
+#' @export
 timeprune <- function(tr,tipdates=NULL,reverse=FALSE){
   tl <- tr$tip.label
   if(!is.null(tipdates)){
@@ -22,11 +27,14 @@ timeprune <- function(tr,tipdates=NULL,reverse=FALSE){
   }
   ntips <- length(tr$tip.label)
   o <- order(td,decreasing=TRUE)
+  tl <- tl[o]
   trs <- list()
-  for(i in 1:(ntips-2)){
-    mintip <- tl[o[i]]
-    tr <- drop.tip(tr,mintip,trim.internal=TRUE)
-    trs[[i]] <- tr
+  for(i in 1:(ntips-1)){
+    trs[[tl[i]]] <- tr
+    if(i<(ntips-1)){
+      mintip <- tl[i]
+      tr <- drop.tip(tr,mintip,trim.internal=TRUE)
+    }
   }
   if(!reverse){
     trs <- rev(trs)
