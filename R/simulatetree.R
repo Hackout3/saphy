2815d7f90c07549c0b928025628c83d757b2d5fc
donkeyshot <don@xs4all.nl>
2016-06-22 21:38:34 +0200

    add function

--- R/simulatetree.R
+++ R/simulatetree.R
@@ -1,12 +1,41 @@
-simulate <- function(N = c(100, 10000),
+#' Simulate tree and sequences in two-population SIR-model
+#'
+#' \code{simulatetree} does a deterministic simulation of a two-population SIR-model,
+#' takes samples and simulates a phylogenetic tree. For all this, it makes use of phydynR
+#' (https://github.com/emvolz-phylodynamics/phydynR). Then it simulates DNA-sequences for these samples, with simSeq (\pkg{phangorn}).
+#'
+#' @param N the population sizes of the two populations
+#' @param params the parameters for the SIR-model: a named list with transmission rate matrix \code{beta},
+#' recovery rate \code{gamma} and death rate \code{mu}. Births balance deaths. Transmission is modelled with frequency-dependent
+#' transmission terms (division by N).
+#' @param I0 initial numbers of infecteds. The rest is assumed susceptible.
+#' @param t1 duration of the simulation
+#' @param res resolution of the epidemic simulation (ODE solver)
+#' @param sampletimes indicates if sampling occurs at random times proportional to incidence (default \code{"incidence"}) or
+#' at random uniform times (\code{"regular"})
+#' @param samplesize the number of samples to be taken
+#' @param sampleperiod the time interval from which the sampling times should be taken
+#' @param sampledist indicates how samples should be divided over the two populations, apart from bias. The default \code{"incidence"} indicates
+#' proportionality to incidence; \code{"popsize"} indicates proportionality to population size; \code{"fixed"} indicates
+#' no proportionality, i.e. fifty-fifty
+#' @param samplebias indicates the extra weight (multiplication factor) given for sampling the first population
+#' relative to the second. If equal to 1, the weight is completely determined by \code{sampledist}.
+#' @param ... arguments to be passed to simSeq (\pkg{phangorn})
+#'
+#' @return A list with a tree (classes \code{phylo} and \code{DatedTree}), and a set of sequences (class \code{phyDat}).
+#'
+#' @author Don Klinkenberg (\email{don@@xs4all.nl})
+#'
+#' @export
+simulatetree <- function(N = c(100, 10000),
                      params = list(beta = matrix(c(2,0,0,2),nrow=2), gamma = 1, mu = .05),
                      I0 = c(1,1),
-                     t1 = 100,
+                     t1 = 100, res = 1000,
                      sampletimes = c("incidence", "regular"),
                      samplesize = 200,
                      sampleperiod = c(0, t1),
                      sampledist = c("incidence", "popsize", "fixed"),
-                     samplebias = 1) {
+                     samplebias = 1, ...) {
 
   ### Build the demographic process, i.e. the transmission model
   dp <- build.demographic.process(
@@ -24,8 +53,8 @@ simulate <- function(N = c(100, 10000),
   ### Make incidence curves rather than prevalence curves
   epicurve <- dp(theta = c(params, list(N = N)),
                 x0 = c(I1 = I0[1], I2 = I0[2], S1 = N[1] - I0[1], S2 = N[2] - I0[2]),
-                t0 = 0, t1 = t1)[[5]]
-  epicurve <- epicurve[epicurve[,1] > sampleperiod[1] & epicurve[,1] < sampleperiod[2] - t1/1000,]
+                t0 = 0, t1 = t1, res = res)[[5]]
+  epicurve <- epicurve[epicurve[,1] > sampleperiod[1] & epicurve[,1] < sampleperiod[2] - t1/res,]
   incfunc <- function(epirow, b) {
     c(sum(epirow[4]*b[,1]*epirow[2:3]), sum(epirow[5]*b[,2]*epirow[2:3]))/N
   }
@@ -52,20 +81,21 @@ simulate <- function(N = c(100, 10000),
   sampleStates[cbind(1:samplesize,sampledpops)] <- 1
 
   ### Add uniform increment to sample times, and sort times and populations
-  sampleTimes <- sampleTimes + runif(samplesize, 0, t1/1000)
+  sampleTimes <- sampleTimes + runif(samplesize, 0, t1/res)
   sampleStates <- sampleStates[order(sampleTimes),]
   sampleTimes <- sort(sampleTimes)
 
+  ### Simulate the phylogenetic tree
   cotree <- sim.co.tree(theta = c(params, list(N = N)),
                         dp,
                         x0 = c(I1 = I0[1], I2 = I0[2], S1 = N[1] - I0[1], S2 = N[2] - I0[2]),
-                        t0 = 0, sampleTimes = sampleTimes,
+                        t0 = 0, res = res, sampleTimes = sampleTimes,
                         sampleStates = sampleStates)
 
-  seqs <- simSeq(cotree)
+  ### Simulate the sequences
+  seqs <- simSeq(cotree, ...)
   return(list(tree = cotree,
-              sequences = seqs,
-              sampledpopulations = sampleStates))
+              sequences = seqs))
 
 }
 
