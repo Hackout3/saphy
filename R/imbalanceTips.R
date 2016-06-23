dad95e1c1a7727f65382f93a1e0b92e9de6870d1
Yu Luo <yu.t.luo@mail.mcgill.ca>
2016-06-23 11:03:47 -0700

    b

---
+++ R/imbalanceTips.R
@@ -0,0 +1,16 @@
+#return suspecious tips
+imbalanceTips<-function(tree,reps=100){
+  treemetrics<-imbalanceMetrics(tree)
+  if(length(which(as.vector(unlist(treemetrics[1:12]))<threthold(tree,reps=100)[2,]
+                  |as.vector(unlist(treemetrics[1:12])>threthold(tree,reps=100)[1,])))>1)
+  {
+    ImbalanceTips<-NULL
+    for(i in 1:tree$Nnode){
+      if(!any(unlist(imbalanceMetrics(timeprune(tree)$trees[[i]]))[1:12]<threthold(timeprune(tree)$trees[[i]],reps=100)[2,]|
+              unlist(imbalanceMetrics(timeprune(tree)$trees[[i]]))[1:12]>threthold(timeprune(tree)$trees[[i]],reps=100)[1,]))
+      {
+        ImbalanceTips<-c(ImbalanceTips,timeprune(tree)$trees[[i]]$tip.label[1])
+      }
+    }
+    return(ImbalanceTips)}
+  else (return("This tree is balanced"))}
